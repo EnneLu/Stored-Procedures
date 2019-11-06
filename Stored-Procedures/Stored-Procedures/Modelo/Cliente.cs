05eae42950db01d2f539e0666d461d842ac953ad
vitoria medeiros <53539926+vitoriasmedeiros@users.noreply.github.com>
2019-11-06 16:45:17 -0300

    DAOCliente + Modelo de Cliente

---
+++ Stored-Procedures/Stored-Procedures/Modelo/Cliente.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Stored_Procedures.Modelo
+{
+    public class Cliente
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public DateTime data_nascimento { get; set; }
+        public string cpf { get; set; }
+
+
+        public Cliente()
+        {
+            this.id = "";
+            this.nome = "";
+            this.data_nascimento = new DateTime();
+            this.cpf = "";
+        }
+
+
+        public Cliente(int id, string nome, DateTime data_nascimento, string cpf)
+        {
+            this.id = id;
+            this.nome = nome;
+            this.data_nascimento = data_nascimento;
+            this.cpf = cpf;
+            
+
+        }
+    }
+}
